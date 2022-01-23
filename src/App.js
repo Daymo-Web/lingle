16f136081464711910fd65be71b6f80381aae12a
jhomatas48 <jhomatas48@gmail.com>
2022-01-23 02:20:44 -0600

    added backend

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
   const [cnt, setCnt] = useState(0);
   const [guesses, setGuesses] = useState([]);
   const [correct, setCorrect] = useState(false);
+  const [wordHash, setWordHash] = useState({});
   const handleChange = (e) => {
     const { maxLength, value, name } = e.target;
     const [fieldName, fieldIndex] = name.split("-");
@@ -104,6 +105,8 @@ function App() {
     const isValid = checkWords(str);
     console.log("isValid", isValid);
     if (isValid) {
+      backend(str);
+      console.log(wordMap);
       console.log("Valid!");
     }
   };
@@ -125,6 +128,82 @@ function App() {
     console.log(word);
     return scrabble.includes(word.toUpperCase()) || words.includes(wordString);
   };
+
+
+  let dictionary = {
+    a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0,
+    n: 0, o: 0, p: 0, q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0, y: 0, z: 0
+  };
+
+  let dictionary2 = {
+    a: [], b: [], c: [], d: [], e: [], f: [], g: [], h: [], i: [], j: [], k: [], l: [], m: [],
+    n: [], o: [], p: [], q: [], r: [], s: [], t: [], u: [], v: [], w: [], x: [], y: [], z: []
+  };
+
+  let wordMap = {};
+
+  const removeItemOnce = (arr, value) => {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  };
+
+  const backend = (wordString) => {
+    for (let i = 0; i < wordString.length; ++i) {
+      let idxStr = i.toString()
+      wordMap[idxStr] = [wordString.charAt(i), ""];
+    }
+    console.log(wordMap);
+    for (let i = 0; i < answer.length; ++i) {
+      dictionary[answer.charAt(i)] += 1;
+    }
+    console.log(dictionary);
+    let tmpDict = dictionary;
+    for (let i = 0; i < wordString.length; ++i) {
+      console.log(wordString.charAt(i));
+      dictionary2[wordString.charAt(i)].push(i);
+    }
+    console.log(dictionary2);
+    for (let i = 0; i < wordString.length; ++i) {
+      let currLetter = wordString.charAt(i);
+      if (wordMap[i][1] == "") {
+        if (tmpDict[currLetter] == 0) {
+          for (let j = 0; j < dictionary2[currLetter].length; ++j) {
+            wordMap[dictionary2[currLetter][j]][1] = "grey";
+          }
+        } else if (dictionary2[currLetter].length == 1) {
+          if (currLetter == answer.charAt(i)) {
+            wordMap[i][1] = "green";
+          } else {
+            wordMap[i][1] = "yellow";
+          }
+        } else {
+          console.log("multiple");
+          console.log(i);
+          let tmpLst = dictionary2[currLetter];
+          for (let j = 0; j < tmpLst.length; ++j) {
+            if (currLetter == answer.charAt(j)) {
+              tmpDict[currLetter] -= 1;
+              tmpLst.removeItemOnce(j);
+              wordMap[i][1] = "green";
+            }
+          }
+          for (let j = 0; j < tmpLst.length; ++j) {
+            if (tmpDict[currLetter] <= 0) {
+              wordMap[i][1] = "grey";
+            } else {
+              tmpDict[currLetter] -= 1;
+              wordMap[i][1] = "yellow";
+            }
+          }
+        }
+      }
+    }
+    setWordHash(wordMap);
+  };
+
   return (
     <div className="App">
       <div>
